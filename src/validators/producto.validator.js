cc5afb49d05700b95376ef742e1206daaaa402e7
rmarchino <rmarchino95@gmail.com>
2023-12-11 11:46:53 -0500

    Agregando modificaciones

--- src/validators/producto.validator.js
+++ src/validators/producto.validator.js
@@ -1,38 +1,50 @@
-const { check } = require('express-validator');
-const validateResult = require('../utils/validate');
-
-
+const { check } = require("express-validator");
+const validateResult = require("../utils/validate");
 
 const createProductoValidator = [
-    check('nombre', 'Error en el campo nombre')
-        .exists().withMessage('nombre es obligatoio')
-        .notEmpty().withMessage('nombre no debe de estar vacío')
-        .isString().withMessage('El tipo de dato debe de ser estring')
-        .isLength({ min: 10 }).withMessage('El nombre debe tener mínimo 10 caracteres'),
+  check("nombre", "Error en el campo nombre")
+    .exists()
+    .withMessage("nombre es obligatoio")
+    .notEmpty()
+    .withMessage("nombre no debe de estar vacío")
+    .isString()
+    .withMessage("El tipo de dato debe de ser estring")
+    .isLength({ min: 10 })
+    .withMessage("El nombre debe tener mínimo 10 caracteres"),
 
-    check('descripcion', 'Error en el campo descripcion')
-        .isString().withMessage('El tipo de dato debe de ser estring')
-        .isLength({ min: 10 }).withMessage('El descripcion debe tener mínimo 10 caracteres'),
-        
-    check('precio', 'Error en el campo precio')
-        .isNumeric().withMessage('El tipo de dato debe ser numérico')
-        .notEmpty().withMessage('precio no debe de estar vacío'),
+  check("descripcion", "Error en el campo descripcion")
+    .isString()
+    .withMessage("El tipo de dato debe de ser estring")
+    .isLength({ min: 10 })
+    .withMessage("El descripcion debe tener mínimo 10 caracteres"),
 
-    check('stock', 'Error en el campo stock')
-        .isNumeric().withMessage('El tipo de dato debe ser numérico')
-        .isInt().withMessage('Tipo de dato debe de ser entero')
-        .notEmpty().withMessage('stock no debe de estar vacío'),
+  check("precio", "Error en el campo precio")
+    .isNumeric()
+    .withMessage("El tipo de dato debe ser numérico")
+    .notEmpty()
+    .withMessage("precio no debe de estar vacío"),
 
-    check('categoria', 'Error en el campo categoria')
-        .isInt().withMessage('Tipo de dato debe de ser entero')
-        .notEmpty().withMessage('categoria no debe de estar vacío'),
+  check("stock", "Error en el campo stock")
+    .isNumeric()
+    .withMessage("El tipo de dato debe ser numérico")
+    .isInt()
+    .withMessage("Tipo de dato debe de ser entero")
+    .notEmpty()
+    .withMessage("stock no debe de estar vacío"),
 
-    check('usuario', 'Error en el campo usuario')
-        .isInt().withMessage('Tipo de dato debe de ser entero')
-        .notEmpty().withMessage('usuario no debe de estar vacío'),
-    
-    validateResult
-]
+  check("categoria", "Error en el campo categoria")
+    .isString()
+    .withMessage("Tipo de dato debe de ser string")
+    .notEmpty()
+    .withMessage("categoria no debe de estar vacío"),
 
+  check("usuario", "Error en el campo usuario")
+    .isInt()
+    .withMessage("Tipo de dato debe de ser entero")
+    .notEmpty()
+    .withMessage("usuario no debe de estar vacío"),
+
+  validateResult,
+];
 
 module.exports = createProductoValidator;
